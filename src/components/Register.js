8a77932a4031797e03f520da47eb125b67663344
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-10-06 19:49:48 -0500

    define component Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,3 +1,37 @@
+import { useState } from 'react'
+
 export function Register() {
-  return <div>Register</div>
+
+  const [user, setUser] = useState({
+    email: '', password: ''
+  })
+
+  const handleChange = ({target: {name, value}}) => {
+    console.log(name, value)
+    setUser({...user, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(user)
+  }
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input 
+          type="email" name="email" 
+          id="email" placeholder="email@example.com" 
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input 
+          type="password" name="password" id="password" 
+          onChange={handleChange}
+        />
+        <button>Register</button>
+      </form>
+    </div>
+  )
 }
